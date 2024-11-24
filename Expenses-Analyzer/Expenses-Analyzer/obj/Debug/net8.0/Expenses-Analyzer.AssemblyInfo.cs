a9c4a4581a2472c984514b675963a1665db3e817
Kaveesha <kaveeshewaduge111@gmail.com>
2024-11-24 13:00:17 +0530

    Create and Update Categories with CategoryController

--- Expenses-Analyzer/Expenses-Analyzer/obj/Debug/net8.0/Expenses-Analyzer.AssemblyInfo.cs
+++ Expenses-Analyzer/Expenses-Analyzer/obj/Debug/net8.0/Expenses-Analyzer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expenses-Analyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9005c1d142ed758bd1a6c5132cc0f419ba52e314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a69c12dfcecbc357b86f2ce9bb0a4cbd994dca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expenses-Analyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expenses-Analyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
