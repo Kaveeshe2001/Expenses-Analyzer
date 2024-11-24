9005c1d142ed758bd1a6c5132cc0f419ba52e314
Kaveesha <kaveeshewaduge111@gmail.com>
2024-11-24 11:03:11 +0530

    Initial Create

---
+++ Expenses-Analyzer/Expenses-Analyzer/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expenses_Analyzer.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column (TypeName="nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Type { get; set; } = "Expense";
+    }
+}
---
+++ Expenses-Analyzer/Expenses-Analyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Expenses_Analyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
