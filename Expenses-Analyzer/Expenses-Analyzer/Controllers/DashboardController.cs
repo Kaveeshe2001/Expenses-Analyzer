79cbdfd0ac0c0efe7f7e47fea5851ef9c151fa83
Kaveesha <kaveeshewaduge111@gmail.com>
2024-11-25 18:49:07 +0530

    DashboardController-Total Income,Expenses and Balance

--- Expenses-Analyzer/Expenses-Analyzer/Controllers/DashboardController.cs
+++ Expenses-Analyzer/Expenses-Analyzer/Controllers/DashboardController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expenses_Analyzer.Data;
+using Expenses_Analyzer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace Expenses_Analyzer.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //Last 7 Days
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            //Total Income
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(J => J.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Total Expense
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(J => J.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Total Income
+            int Balance = TotalIncome - TotalExpense;
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture, "{0:C0}",Balance);
+
             return View();
         }
     }
